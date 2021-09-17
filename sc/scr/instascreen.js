65980a5219a7752f1b6976ceef91b6ccd7e426e7
varnitha2008 <72798257+varnitha2008@users.noreply.github.com>
2021-09-17 18:01:51 +0530

    Add files via upload

---
+++ sc/scr/instascreen.js
@@ -0,0 +1,14 @@
+import React,{Component} from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class INSTAscreen extends Component{
+  render(){
+    return(
+      <View>
+          <Text>
+              INSTAGRAM
+          </Text>
+    </View>
+    )
+  }
+}
